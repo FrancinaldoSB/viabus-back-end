663d1bed5567e9faca350717c962474796ffead0
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2024-08-09 00:22:59 -0300

    feat: Adicionar rotas de clientes e telefones

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Hono } from 'hono'
+import { Hono, Context } from "hono";
+import customersRoute from "./routes/customers";
+import phonesRoute from "./routes/phones";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/api/customers", customersRoute);
+app.route("/api/phones", phonesRoute);
 
-export default app
+export default app;
